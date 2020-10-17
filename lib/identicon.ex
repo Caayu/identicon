c4238a20e9a96f05b397a3af2cc5a0f96175a619
caayu <henrycay@outlook.com>
2020-10-17 17:45:28 -0300

    create function to buid grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,19 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
